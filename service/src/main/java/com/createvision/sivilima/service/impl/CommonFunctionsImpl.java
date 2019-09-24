22a8fc13145690625733e8d166c7513cba7eb59a
Pasan Madusanka <pasan@seneru.com>
2019-09-24 14:27:46 +0530

    add new class to common function

---
+++ service/src/main/java/com/createvision/sivilima/service/impl/CommonFunctionsImpl.java
@@ -0,0 +1,23 @@
+package com.createvision.sivilima.service.impl;
+
+import org.springframework.stereotype.Repository;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Repository("commonFunctionsImpl")
+public class CommonFunctionsImpl {
+
+    public Date getCurrentTime(String timeZone)throws Exception{
+        LocalDateTime today = LocalDateTime.now();
+        ZoneId id = ZoneId.of(timeZone);  //Create timezone
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(today, id);  //Add timezone to
+        String formattedDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(zonedDateTime);
+        Date date=new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(formattedDateTime);
+        return date;
+    }
+}
