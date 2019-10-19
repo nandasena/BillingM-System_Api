96a30aa67b10a04b68b4b210e45201ff3442893c
Pasan Madusanka <pasan@seneru.com>
2019-10-19 17:00:35 +0530

    add new table called item_code

---
+++ dao/src/main/java/com/createvision/sivilima/tableModel/ItemCode.java
@@ -0,0 +1,31 @@
+package com.createvision.sivilima.tableModel;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "item_code")
+public class ItemCode extends BaseObject implements Serializable {
+
+    @Basic
+    @Column(name = "code_name")
+    private String codeName;
+
+    public String getCodeName() { return codeName; }
+    public void setCodeName(String codeName) { this.codeName = codeName;}
+
+    @Basic
+    @Column(name = "code")
+    private String code;
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code;}
+
+    @Basic
+    @Column(name = "last_number")
+    private int lastNumber;
+    public int getLastNumber() { return lastNumber; }
+    public void setLastNumber(int lastNumber) { this.lastNumber = lastNumber; }
+}
