d7700325ab4588d7e540db30008293147f9e717d
Pasan Madusanka <pasan@seneru.com>
2019-12-21 17:42:57 +0530

    change file name in store procedure

--- db_scripts/getPurchaseOrderByDate.sql
+++ db_scripts/getPurchaseOrderByDate.sql
@@ -7,17 +7,17 @@ BEGIN
 
 SELECT  po.id ,
         po.purchase_code,
-        sp.name,
+        sp.name AS supplier_name,
         sp.address_1,
         sp.address_2,
-        us.name,
+        us.name AS user_name,
         po.estimate_receive_date,
         po.total_amount,
         po.total_discount,
         us.id AS user_id,
         sp.supplier_id AS supplier_is
 FROM (SELECT * FROM purchase_order pro
-WHERE DATE( pro.created_at)>= form_date AND Date(pro.created_at)<to_date) AS po
+WHERE DATE( pro.created_at)>= form_date AND Date(pro.created_at)<=to_date) AS po
 LEFT JOIN supplier sp ON sp.supplier_id =po.fk_supplier_id
 LEFT JOIN users us ON po.fk_user_id = us.id;
 
