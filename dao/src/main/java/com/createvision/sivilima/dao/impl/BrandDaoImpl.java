93726890463e38516efd27351c9a0333016dfea7
Pasan Madusanka <pasan@seneru.com>
2020-11-06 21:41:56 +0530

    change repository name.

--- dao/src/main/java/com/createvision/sivilima/dao/impl/BrandDaoImpl.java
+++ dao/src/main/java/com/createvision/sivilima/dao/impl/BrandDaoImpl.java
@@ -4,6 +4,6 @@ import com.createvision.sivilima.dao.BrandDao;
 import com.createvision.sivilima.tableModel.Brand;
 import org.springframework.stereotype.Repository;
 
-@Repository("branchDao")
+@Repository("branDao")
 public class BrandDaoImpl extends AbstractDaoImpl<Brand,Long> implements BrandDao {
 }
