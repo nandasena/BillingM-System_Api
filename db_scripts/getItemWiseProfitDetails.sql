c1f0976af577c3bc559c426afe2e35d984071213
Pasan Madusanka <pasan@seneru.com>
2021-04-24 17:37:27 +0530

    remove schema from stored procedure

--- db_scripts/getItemWiseProfitDetails.sql
+++ db_scripts/getItemWiseProfitDetails.sql
@@ -13,7 +13,7 @@ from (select date (created_at),
      IFNULL(sum(total_amount), 0) as total_amount,
      IFNULL(sum(total_item_discount), 0) as total_item_discount,
      IFNULL(sum(item_cost*selling_quantity), 0) as item_cost,
-     item_id from sivilima.invoice_item_Details
+     item_id from invoice_item_Details
 where created_at >= form_date
   and created_at <= to_date
 group by item_id
